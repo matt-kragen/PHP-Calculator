3fa50eb8f7a6d65ca9fc611281954cd1c59f5e83
Matt Kragen <56685055+matt-kragen@users.noreply.github.com>
2022-10-19 14:52:49 -0700

    Refactor to use setUp()

--- tests/CalculatorTest.php
+++ tests/CalculatorTest.php
@@ -1,36 +1,32 @@
 <?php
 // run "./vendor/bin/phpunit" in Terminal
+use PHPUnit\Framework\TestCase;
 
 class CalculatorTest extends \PHPUnit\Framework\TestCase {
-    public function testAdd()
-    {
-        $calculator = new App\Calculator;
-        $result = $calculator->add(20,5);
 
+    private $calculator;
+    
+    public function setUp(): void {
+        $this->calculator = new App\Calculator();
+    }
+    
+    public function testAdd(): void {
+        $result = $this->calculator->add(20,5);
         $this->assertEquals(25, $result);
     }
 
-    public function testSubtract()
-    {
-        $calculator = new App\Calculator;
-        $result = $calculator->subtract(15, 5);
-
+    public function testSubtract(): void {
+        $result = $this->calculator->subtract(15, 5);
         $this->assertEquals(10, $result);
     }
 
-    public function testMultiply()
-    {
-        $calculator = new App\Calculator;
-        $result = $calculator->multiply(2, 3);
-
+    public function testMultiply(): void {
+        $result = $this->calculator->multiply(2, 3);
         $this->assertEquals(6, $result);
     }
 
-    public function testDivide()
-    {
-        $calculator = new App\Calculator;
-        $result = $calculator->divide(24, 2);
-
+    public function testDivide(): void {
+        $result = $this->calculator->divide(24, 2);
         $this->assertEquals(12, $result);
     }
 }
